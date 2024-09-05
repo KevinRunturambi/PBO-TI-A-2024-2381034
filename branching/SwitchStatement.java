7e7dbccaf2197a5ff5d6096dece664e2e0ca6313
KevinRunturambi <2381034@unai.edu>
2024-09-05 10:31:45 +0700

    Handson1

--- branching/SwitchStatement.java
+++ branching/SwitchStatement.java
@@ -3,12 +3,13 @@ package branching;
 import java.util.Scanner;
 
 public class SwitchStatement {
-
+    public class IfStatement {
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner( source System.in);
 
         System.out.println("Masukkan nomor hari (1 - 7) ; );
-        int nomorHari = input.nextInt();
+        int nomorHari = input.nextInt[];
 
         String namaHari = "";
 
